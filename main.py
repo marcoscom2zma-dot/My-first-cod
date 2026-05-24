f2db476c73a4811299969f330b81e495ee975b9c
marcoscom2zma-dot <marcos.com.2z.ma@gmail.com>
2026-05-24 15:49:46 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+
+# --- CÓDIGO DE DEMONSTRAÇÃO ---
+# Este é um script simples em Python para demonstrar o uso do GitHub.
+
+def saudar_usuario(nome):
+    mensagem = f"Olá, {nome}! Seja muito bem-vindo ao meu primeiro projeto no GitHub."
+    return mensagem
+
+# Execução do código
+if __name__ == "__main__":
+    nome_visitante = "Visitante"
+    print(saudar_usuario(nome_visitante))
+
